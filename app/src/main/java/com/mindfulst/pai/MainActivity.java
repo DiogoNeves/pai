fbe6c443662b83d3dd624d62cca9024afb548068
Diogo Neves <diogo.neves@gmail.com>
2015-04-28 21:34:25 +0100

    Integrated wit messaging demo

--- app/src/main/java/com/mindfulst/pai/MainActivity.java
+++ app/src/main/java/com/mindfulst/pai/MainActivity.java
@@ -6,21 +6,49 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
-public class MainActivity extends ActionBarActivity {
+import java.io.IOException;
+import java.util.ArrayList;
+
+import ai.wit.sdk.IWitListener;
+import ai.wit.sdk.Wit;
+import ai.wit.sdk.model.WitOutcome;
+
+
+public class MainActivity extends ActionBarActivity implements IWitListener {
+
+    Wit witApi;
+    TextView sentence;
+    TextView witResults;
+    Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final String accessToken = "W2HI75IAJB2Y5RTAPXPEDH4TEPQ6NZ6K";
+        witApi = new Wit(accessToken, this);
+        witApi.enableContextLocation(getApplicationContext());
+
+        sentence = (TextView) findViewById(R.id.outSentence);
+
+        witResults = (TextView) findViewById(R.id.outIntentDebug);
+        witResults.setMovementMethod(new ScrollingMovementMethod());
+
+        gson = new GsonBuilder().setPrettyPrinting().create();
 
         TextView whatInput = (TextView) findViewById(R.id.inWhat);
         final NotificationManager notifier =
@@ -61,5 +89,44 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void toggleListening(View v) {
+        try {
+            witApi.toggleListening();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void witDidGraspIntent(ArrayList<WitOutcome> witOutcomes, String s, Error error) {
+        if (error != null) {
+            witResults.setText(error.getLocalizedMessage());
+        } else {
+            String output = gson.toJson(witOutcomes);
+            witResults.setText(output);
+            sentence.setText(witOutcomes.get(0).get_text());
+        }
+    }
+
+    @Override
+    public void witDidStartListening() {
+        sentence.setText("Witting");
+    }
+
+    @Override
+    public void witDidStopListening() {
+        sentence.setText("Processing");
+    }
+
+    @Override
+    public void witActivityDetectorStarted() {
+        sentence.setText("Listening");
+    }
+
+    @Override
+    public String witGenerateMessageId() {
+        return null;
     }
 }
