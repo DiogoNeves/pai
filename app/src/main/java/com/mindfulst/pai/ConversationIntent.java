a981d7e981cf07139f135e813ef29b35194721d8
Diogo Neves <diogo.neves@gmail.com>
2015-04-29 13:59:44 +0100

    Converts witoucome to conversation intent

--- app/src/main/java/com/mindfulst/pai/ConversationIntent.java
+++ app/src/main/java/com/mindfulst/pai/ConversationIntent.java
@@ -1,6 +1,12 @@
 package com.mindfulst.pai;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+
+import java.util.HashMap;
 import java.util.Map;
+
+import ai.wit.sdk.model.WitOutcome;
 
 /**
  * Wrapper for Wit intents.
@@ -14,5 +20,25 @@ public final class ConversationIntent {
         this.intent = intent;
         this.statement = statement;
         this.data = data;
+    }
+
+    public static ConversationIntent createFrom(WitOutcome outcome) {
+        String intent = outcome.get_intent();
+        String statement = outcome.get_text();
+
+        // TODO: Improve this with a list of data extractors
+        Map<String, String> data = new HashMap<>();
+        if (!intent.equals("UNKNOWN")) {
+            Map<String, JsonElement> entities = outcome.get_entities();
+            for (String k : entities.keySet()) {
+                JsonArray values = entities.get(k).getAsJsonArray();
+                if (values.size() > 0) {
+                    JsonElement valueElement = values.get(0);
+                    data.put(k, valueElement.getAsString());
+                }
+            }
+        }
+
+        return new ConversationIntent(intent, statement, data);
     }
 }
